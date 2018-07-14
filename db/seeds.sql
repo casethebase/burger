9aa522dd958d74bb43a51e2417246aa0d70fe052
casethebase <trahan.casey.c@gmail.com>
2018-07-14 08:06:56 -0500

    Initial Files, works locally

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Rick Astleys Best', FALSE, now());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Never Gonna Ground You Up', FALSE, now());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Never Gonna Meat You Down', FALSE, now());
---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var app = express();
+var serverUp = false;
+
+// This says that if we do root or /, we mean to look in the public folder.
+app.use(express.static('public'));
+
+var PORT = process.env.PORT || 3000;
+var exphbs = require('express-handlebars');
+
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+var routes = require('./controllers/burgers_controller.js');
+app.use('/', routes);
+
+if (serverUp = false) {
+    app.listen(PORT);
+    console.log('App Listening on port: ' + PORT);
+} else {serverUp = false};
